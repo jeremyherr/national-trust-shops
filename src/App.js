c9b79485ce1c4b0991770f6e5d1769ac52f634bc
Jeremy Herr <jeremy.herr@gmail.com>
2017-12-02 20:47:45 +0000

    Added communication between input form component and RoughDistances display

--- src/App.js
+++ src/App.js
@@ -1,19 +1,85 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import EventEmitter from 'eventemitter3';
 
 class App extends Component {
+  constructor(properties) {
+    super(properties);
+    this.ee = new EventEmitter();
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Find National Trust Shops near you</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <PostcodeForm ee={this.ee} />
+        <RoughDistances ee={this.ee} />
       </div>
+    );
+  }
+}
+
+class PostcodeForm extends React.Component {
+  constructor(properties) {
+    super(properties);
+    this.state = {value: ''};
+    this.ee = properties.ee;
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+    this.ee.emit('postcode', [event.target.value]);
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Enter your postcode:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+class RoughDistances extends React.Component {
+  constructor(properties) {
+    super(properties);
+
+    this.ee = properties.ee;
+
+    this.state = {
+      postcode: ''
+    };
+  }
+
+  updatePostcode(postcode) {
+    this.setState({postcode: postcode});
+  }
+
+  componentDidMount() {
+    this.ee.on('postcode', this.updatePostcode.bind(this));
+  }
+
+  componentWillUnmount() {
+    this.ee.removeListener('postcode', this.updatePostcode.bind(this));
+  }
+
+  render() {
+    return (
+      <p>You entered: {this.state.postcode}</p>
     );
   }
 }
